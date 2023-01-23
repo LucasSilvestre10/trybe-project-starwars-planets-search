7f2e8e87e243b669336bf1564aa6572c79994ee5
Lucas Silvestre <lucas_silvestre.10@hotmail.com>
2023-01-23 16:25:37 -0300

    requisito1(criado tabela com loading de aguardo para a requisição da api)

---
+++ src/context/PlanetsProvider.js
@@ -0,0 +1,39 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { createContext, useEffect, useMemo, useState } from 'react';
+import useFetch from '../hooks/useFetch';
+
+export const PlanetsContext = createContext();
+
+function PlanetsProvider({ children }) {
+  const { isLoading, makeFetch, errors } = useFetch();
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    async function fetching(url) {
+      const data = await makeFetch(url);
+
+      const cleanedPlanets = data.results.map((planet) => {
+        delete planet.residents;
+        return planet;
+      });
+
+      setPlanets(cleanedPlanets);
+    }
+
+    fetching('https://swapi.dev/api/planets');
+  }, []);
+
+  const values = useMemo(() => (
+    { isLoading, errors, planets }
+  ), [isLoading, errors, planets]);
+
+  return (
+    <PlanetsContext.Provider value={ values }>
+      { children }
+    </PlanetsContext.Provider>
+  );
+}
+
+PlanetsProvider.propTypes = {}.isRequired;
+
+export default PlanetsProvider;
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+function useFetch() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [errors, setErrors] = useState(null);
+
+  const makeFetch = async (url) => {
+    try {
+      setIsLoading(true);
+
+      const response = await fetch(url);
+
+      const json = await response.json();
+
+      return json;
+    } catch (error) {
+      setErrors(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return {
+    makeFetch, isLoading, errors,
+  };
+}
+
+export default useFetch;
